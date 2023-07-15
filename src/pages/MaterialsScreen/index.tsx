8f4635ad4778a469a24bde22149653b2400cc5a2
Andriy Plyska <andriyplyska@MBP-Andriy.home>
2023-07-15 23:25:15 +0200

    initial commit

---
+++ src/pages/MaterialsScreen/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const MaterialsScreen: React.FC = () => {
+    return (
+        <div>MaterialsScreen</div>
+    )
+}
+
+export default MaterialsScreen;
---
+++ src/pages/TasksScreen/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const TasksScreen: React.FC = () => {
+    return (
+        <div>TasksScreen</div>
+    )
+}
+
+export default TasksScreen;
